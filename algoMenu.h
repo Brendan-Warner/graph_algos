61cace030503fa5f825851c6b97f1d12ea2dd02b
Brendan-Warner <80108372+Brendan-Warner@users.noreply.github.com>
2022-12-20 18:03:44 -0600

    Update algoMenu.h

--- algoMenu.h
+++ algoMenu.h
@@ -1,13 +1,9 @@
 #pragma once
 
 
-//more functionaly will be added later, such as a function for getting walls and for other algos
+
 
 
 void menuFunc();
 
 
-/*can change back to
-#ifndef ALGOMENU_H
-#endif
-if need to later*/
